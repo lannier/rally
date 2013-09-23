911fe151185b6fd849f1ddc03dc832177ed77d4e
xchang <xchang.chn@gmail.com>
2013-09-23 16:49:24 +0800

    [S01F01] init repo with rails 4 skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rally::Application.config.secret_key_base = '90175f9c652b73d250543090c6f000e23e84ed05c5d6ead9cdddf4d371c652dc0ac4ede9283d5ace80cd3767142eed8647e79fb2a12b8b788b5eb06d27bef8d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rally::Application.config.session_store :cookie_store, key: '_rally_session'
